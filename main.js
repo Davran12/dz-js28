7e4c1cebe7518a00dc027c72d40a740a9b457322
Davran <davranlapa@gmail.com>
2025-04-01 23:42:15 +0600

    dz31

--- main.js
+++ main.js
@@ -1,65 +1,22 @@
-// ? задание №1
-const phone = "+996 (550) 225-456"
-let phoneCountry
-
-if (phone.startsWith("+996")) {
-  phoneCountry = "KG"
-} else if (phone.startsWith("+7")) {
-  phoneCountry = "KZ"
-} else if (phone.startsWith("+1")) {
-  phoneCountry = "US"
-} else {
-  phoneCountry = "Unknown"
+//! Задание№1
+let number = 1
+for (let i = 0; i < 7; i++) {
+  console.log(number)
+  number *= 2
 }
+//! Задание№2
+const arr = ["canon", "polaroid", "nikon", "sony", "fujifilm", "pentax"]
 
-console.log(phoneCountry)
-
-// ? задание №2
-
-let year = 1995
-let isAlmostLeapYear
-
-if (
-  (year % 4 === 0 && year % 100 !== 0) ||
-  (year % 4 === 3 && (year + 1) % 4 === 0)
-) {
-  isAlmostLeapYear = true
-} else {
-  isAlmostLeapYear = false
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
 }
-
-console.log(isAlmostLeapYear)
-
-let year2 = 1999
-let isAlmostLeapYear2
-
-if (
-  (year2 % 4 === 0 && year2 % 100 !== 0) ||
-  (year2 % 4 === 3 && (year2 + 1) % 4 === 0)
-) {
-  isAlmostLeapYear2 = true
-} else {
-  isAlmostLeapYear2 = false
+//! Задание№3
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i].length)
 }
-
-console.log(isAlmostLeapYear2)
-
-// ? задание №3
-let num = 7
-let result
-
-switch (true) {
-  case num % 7 === 0 && num % 5 !== 0:
-    result = "Семёрка!"
-    break
-  case num % 5 === 0 && num % 7 !== 0:
-    result = "Пятёрка!"
-    break
-  case num % 5 === 0 && num % 7 === 0:
-    result = "Семь и пять!"
-    break
-  default:
-    result = "Обычное число"
+//! Задание№4
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].length > 5) {
+    console.log(arr[i])
+  }
 }
-
-console.log(result)
