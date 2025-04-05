ccd573b8a1ea94ac42a8cfc221b26ad520346841
Davran <davranlapa@gmail.com>
2025-04-05 23:42:10 +0600

    dz33

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-//! Задание№1
-let number = 1
-for (let i = 0; i < 7; i++) {
-  console.log(number)
-  number *= 2
-}
-//! Задание№2
-const arr = ["canon", "polaroid", "nikon", "sony", "fujifilm", "pentax"]
+let number = prompt("Введите число:")
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i])
+if (number % 2 === 0) {
+  if (number % 4 === 0) {
+    console.log("Число четное и делится на 4")
+  } else {
+    console.log("Число четное, но не делится на 4")
+  }
+} else {
+  if (number % 9 === 0) {
+    console.log("Число нечетное и делится на 9")
+  } else {
+    console.log("Число нечетное и не делится на 9")
+  }
 }
-//! Задание№3
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i].length)
-}
-//! Задание№4
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i].length > 5) {
-    console.log(arr[i])
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz")
+  } else if (i % 3 === 0) {
+    console.log("Fizz")
+  } else if (i % 5 === 0) {
+    console.log("Buzz")
+  } else {
+    console.log(i)
   }
 }
