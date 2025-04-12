28503afbcb8bf51756e32a096c63b21f773ee554
Davran <davranlapa@gmail.com>
2025-04-12 22:20:51 +0600

    dz36

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // ! Задание №1
-function heron(a, b, c) {
-  var s = (a + b + c) / 2
+function sum1(a, b, c) {
+  const s = (a + b + c) / 2
   return Math.sqrt(s * (s - a) * (s - b) * (s - c))
 }
-console.log(heron(3, 4, 5))
+console.log(sum1(3, 4, 5))
 // ! Задание №2
-function Kata(a, e) {
-  let i = a.indexOf(e)
+function sum(a, b) {
+  let i = a.indexOf(b)
   return i + 1 ? i : "Not found"
 }
-console.log(Kata([1, 2, 3], 2))
-console.log(Kata([1, 2, 3], 5))
+console.log(sum([1, 2, 3], 2))
+console.log(sum([1, 2, 3], 5))
