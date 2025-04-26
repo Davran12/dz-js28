a319cf3720a87e26cbd78bce0f41a2fab5bc3986
Davran <davranlapa@gmail.com>
2025-04-26 23:59:18 +0600

    dz41

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-// ! Задание №1
-function sum1(a, b, c) {
-  const s = (a + b + c) / 2
-  return Math.sqrt(s * (s - a) * (s - b) * (s - c))
+function count(arr) {
+  const result = {}
+
+  for (let num of arr) {
+    if (result[num]) {
+      result[num] += 1
+    } else {
+      result[num] = 1
+    }
+  }
+
+  return result
 }
-console.log(sum1(3, 4, 5))
-// ! Задание №2
-function sum(a, b) {
-  let i = a.indexOf(b)
-  return i + 1 ? i : "Not found"
-}
-console.log(sum([1, 2, 3], 2))
-console.log(sum([1, 2, 3], 5))
+
+console.log(count([1, 2, 2, 3, 1, 1, 4, 2, 5]))
