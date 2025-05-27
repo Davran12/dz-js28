d269748cd9bef1c967b7c5712e13237067ac1ae7
Davran <davranlapa@gmail.com>
2025-05-27 22:56:39 +0600

    dz

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const paragraphs = document.querySelectorAll("p")
 
-paragraphs.forEach((parag) => {
-  if (parag.textContent === "Click me") {
-    parag.addEventListener("click", () => {
-      console.log("Button clicked!")
-    })
-  }
-})
