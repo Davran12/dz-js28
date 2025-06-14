3ddf7379bd272d92282b60ad505629ef21b4dd26
Davran <davranlapa@gmail.com>
2025-06-14 22:00:56 +0600

    dz60

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+// function createPasswordChecker(expectedPassword) {
+//   return function (enteredPassword) {
+//     return enteredPassword === expectedPassword
+//   }
+// }
 
+// const checkPassword = createPasswordChecker("secret123")
+// console.log(checkPassword("hello"))
+// console.log(checkPassword("secret123"))
+//----------------------------------------------------
+// function startCounter(max, interval) {
+//   let count = 1
+//   const timerId = setInterval(() => {
+//     console.log(count)
+//     if (count === max) {
+//       clearInterval(timerId)
+//     }
+//     count++
+//   }, interval)
+// }
+
+// startCounter(5, 1000)
+//--------------------------------------------
+function countdown(seconds) {
+  const timerId = setInterval(() => {
+    if (seconds > 0) {
+      console.log(seconds)
+      seconds--
+    } else {
+      console.log("Время вышло!")
+      clearInterval(timerId)
+    }
+  }, 1000)
+}
+
+countdown(3)
