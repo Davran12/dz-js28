fa16159fd289dcef74502c894b96ac59685d4426
Davran <davranlapa@gmail.com>
2025-06-26 09:47:26 +0600

    dz65

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-// function createPasswordChecker(expectedPassword) {
-//   return function (enteredPassword) {
-//     return enteredPassword === expectedPassword
-//   }
-// }
-
-// const checkPassword = createPasswordChecker("secret123")
-// console.log(checkPassword("hello"))
-// console.log(checkPassword("secret123"))
-//----------------------------------------------------
-// function startCounter(max, interval) {
-//   let count = 1
-//   const timerId = setInterval(() => {
-//     console.log(count)
-//     if (count === max) {
-//       clearInterval(timerId)
-//     }
-//     count++
-//   }, interval)
-// }
-
-// startCounter(5, 1000)
-//--------------------------------------------
-function countdown(seconds) {
-  const timerId = setInterval(() => {
-    if (seconds > 0) {
-      console.log(seconds)
-      seconds--
-    } else {
-      console.log("Время вышло!")
-      clearInterval(timerId)
+function fetchWithRetry(fetchFunction, retries) {
+  return new Promise((resolve, reject) => {
+    function attempt(remainingTries) {
+      fetchFunction()
+        .then(resolve)
+        .catch((error) => {
+          if (remainingTries === 0) {
+            reject("Не удалось загрузить")
+          } else {
+            setTimeout(() => attempt(remainingTries - 1), 1000)
+          }
+        })
     }
-  }, 1000)
+
+    attempt(retries)
+  })
 }
 
-countdown(3)
+const mockFetch = () =>
+  new Promise((res, rej) =>
+    Math.random() > 0.3 ? res("Данные получены") : rej("Ошибка сети")
+  )
+
+fetchWithRetry(mockFetch, 3)
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error))
+
+// ! -----------------------------------------------------------
+
+function timeoutPromise(promise, ms) {
+  const timeout = new Promise((_, reject) =>
+    setTimeout(() => reject("Таймаут"), ms)
+  )
+
+  return Promise.race([promise, timeout])
+}
+
+const slowPromise = new Promise((res) => setTimeout(() => res("Успех!"), 2000))
+
+timeoutPromise(slowPromise, 1500).then(console.log).catch(console.error)
